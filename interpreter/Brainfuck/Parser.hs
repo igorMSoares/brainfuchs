2e3346c41a2b40cc6e7cee21dc698b3f7a190a0c
Bruno Trigueiro <bruno.trigueiro@outlook.com>
2025-09-19 01:52:28 -0300

    refactor: rename and restructure parsing functions for clarity and improved error handling

--- interpreter/Brainfuck/Parser.hs
+++ interpreter/Brainfuck/Parser.hs
@@ -9,14 +9,14 @@ parse :: String -> Either ParseError Program
 parse s =
   let cleaned = filter (`elem` "><+-.,[]") s
       indexed = zip [1 ..] cleaned
-   in case parse' indexed of
-        Right (prog, []) -> Right prog -- Success if the whole string is consumed
-        Right (_, rest) -> Left (UnmatchedBracket (snd (head rest)) (fst (head rest))) -- Error if there are leftover characters (unmatched ']')
+   in case parseTopLevel indexed of
+        Right (prog, []) -> Right prog 
+        Right (_, (col, c) : _) -> Left (UnmatchedBracket c col)
         Left err -> Left err
 
-parse' :: [(Int, Char)] -> Either ParseError (Program, [(Int, Char)])
-parse' [] = Right ([], [])
-parse' ((col, c) : cs) =
+parseTopLevel :: [(Int, Char)] -> Either ParseError (Program, [(Int, Char)])
+parseTopLevel [] = Right ([], [])
+parseTopLevel stream@((col, c) : cs) =
   case c of
     '>' -> f IncrPtr
     '<' -> f DecrPtr
@@ -24,16 +24,33 @@ parse' ((col, c) : cs) =
     '-' -> f DecrByte
     '.' -> f Output
     ',' -> f Input
+    ']' -> Left (UnmatchedBracket ']' col)
     '[' -> do
-      (loopProg, afterLoop) <- parse' cs
-      case afterLoop of
-        (_, ']') : rest -> do
-          (prog, remaining) <- parse' rest
-          Right (Loop loopProg : prog, remaining)
-        _ -> Left (UnmatchedBracket '[' col) 
-    ']' -> Right ([], cs)
-    _ -> parse' cs
+      (loopProg, afterLoop) <- parseLoopBody cs
+      (restProg, remaining) <- parseTopLevel afterLoop
+      Right (Loop loopProg : restProg, remaining)
   where
     f instr = do
-      (prog, remaining) <- parse' cs
+      (prog, remaining) <- parseTopLevel cs
       Right (instr : prog, remaining)
+
+parseLoopBody :: [(Int, Char)] -> Either ParseError (Program, [(Int, Char)])
+parseLoopBody [] = Left MismatchedBrackets -- Reached EOF inside a loop (unmatched '[')
+parseLoopBody stream@((col, c) : cs) =
+  case c of
+    ']' -> Right ([], cs) 
+    '[' -> do
+      (nestedProg, afterNested) <- parseLoopBody cs
+      (restProg, remaining) <- parseLoopBody afterNested
+      Right (Loop nestedProg : restProg, remaining)
+    '>' -> f IncrPtr
+    '<' -> f DecrPtr
+    '+' -> f IncrByte
+    '-' -> f DecrByte
+    '.' -> f Output
+    ',' -> f Input
+  where
+    f instr = do
+      (prog, remaining) <- parseLoopBody cs
+      Right (instr : prog, remaining)
+
