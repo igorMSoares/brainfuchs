a20357921afe9b3b1dfc6dde93a94d72d833aa21
Arthur Duvareski <arthurdrfabri@gmail.com>
2025-10-14 20:28:26 -0300

    feat: Implementa testes de propriedade para a AST

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -2,10 +2,12 @@ import Test.Hspec
 import Test.Hspec.QuickCheck (modifyMaxSuccess)
 import qualified ParserProps
 import qualified EvaluatorProps
+import qualified AstProps
 
 main :: IO ()
 main = hspec $ do
   modifyMaxSuccess (const 2000) (do
       describe "Parser (QuickCheck)" ParserProps.spec
       describe "Evaluator (QuickCheck)" EvaluatorProps.spec
+      describe "AST (QuickCheck)" AstProps.spec
     )
