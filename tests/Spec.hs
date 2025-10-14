45af3ba6edc4c761084d99e2159d4838664b9473
Leonardo Aoki <leonardoyaoki@gmail.com>
2025-10-14 20:51:14 -0300

    feat: adiciona testes de integração do Assembler e registra no test-suite (cabal/Spec.hs)

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -1,13 +1,15 @@
 import Test.Hspec
 import Test.Hspec.QuickCheck (modifyMaxSuccess)
-import qualified ParserProps
-import qualified EvaluatorProps
+import qualified AssemblerSpec
 import qualified AstProps
+import qualified EvaluatorProps
+import qualified ParserProps
 
 main :: IO ()
 main = hspec $ do
   modifyMaxSuccess (const 2000) (do
-      describe "Parser (QuickCheck)" ParserProps.spec
-      describe "Evaluator (QuickCheck)" EvaluatorProps.spec
+      describe "AssemblerSpec" AssemblerSpec.spec
       describe "AST (QuickCheck)" AstProps.spec
+      describe "Evaluator (QuickCheck)" EvaluatorProps.spec
+      describe "Parser (QuickCheck)" ParserProps.spec
     )
